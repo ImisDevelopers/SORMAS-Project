5f3c029083eb4c2d995548ae38527749a0d0dc1f
Michael Sprauer <Michael.Sprauer@sap.com>
2020-08-09 18:20:32 +0200

    deploy to google cloud

---
+++ Dockerfile
@@ -0,0 +1,39 @@
+FROM maven:3-openjdk-11 as build
+ENV MAVEN_OPTS="-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn"
+# cache dependencies
+COPY ./sormas-api/pom.xml /usr/src/app/sormas-api/pom.xml
+COPY ./sormas-backend/pom.xml /usr/src/app/sormas-backend/pom.xml
+COPY ./sormas-base/pom.xml /usr/src/app/sormas-base/pom.xml
+COPY ./sormas-ear/pom.xml /usr/src/app/sormas-ear/pom.xml
+COPY ./sormas-rest/pom.xml  /usr/src/app/sormas-rest/pom.xml
+COPY ./sormas-serverlibs/ /usr/src/app/sormas-serverlibs/
+COPY ./sormas-ui/pom.xml /usr/src/app/sormas-ui/pom.xml
+COPY ./sormas-widgetset/pom.xml /usr/src/app/sormas-widgetset/pom.xml
+RUN mvn -f /usr/src/app/sormas-base/pom.xml verify clean --fail-never -B
+RUN mvn -f /usr/src/app/sormas-serverlibs/pom.xml package --fail-never -B
+
+# copy sources
+COPY ./sormas-api /usr/src/app/sormas-api
+COPY ./sormas-backend /usr/src/app/sormas-backend
+COPY ./sormas-base /usr/src/app/sormas-base
+COPY ./sormas-ear /usr/src/app/sormas-ear
+COPY ./sormas-rest /usr/src/app/sormas-rest
+COPY ./sormas-ui /usr/src/app/sormas-ui
+COPY ./sormas-widgetset /usr/src/app/sormas-widgetset
+
+# build
+RUN cd /usr/src/app/sormas-base && mvn install -DskipTests=true
+
+FROM payara/server-full:5.2020.3-jdk11
+ENV DOMAIN_NAME=production DOMAIN_DIR=${PAYARA_DIR}/glassfish/domains/${DOMAIN_NAME}
+RUN mkdir -p $DOMAIN_DIR
+COPY sormas-base/setup/sormas.properties ${DOMAIN_DIR}
+COPY sormas-base/setup/logback.xml ${DOMAIN_DIR}/config
+COPY sormas-cargo/init-domain.sh ${SCRIPT_DIR}/init_0_domain.sh
+COPY --from=build /usr/src/app/sormas-serverlibs/target/sormas-serverlibs-libjar.jar ${DOMAIN_DIR}/lib/sormas-serverlibs-libjar.jar
+COPY --from=build /usr/src/app/sormas-ear/target/sormas-ear.ear $DEPLOY_DIR
+COPY --from=build /usr/src/app/sormas-ui/target/sormas-ui.war $DEPLOY_DIR
+COPY --from=build /usr/src/app/sormas-rest/target/sormas-rest.war $DEPLOY_DIR
+
+EXPOSE 6080
+EXPOSE 6048
---
+++ sormas-cargo/init-domain.sh
@@ -0,0 +1,33 @@
+
+# General domain settings
+echo "delete-jvm-options -Xmx512m" >> $POSTBOOT_COMMANDS
+echo "create-jvm-options -Xmx4096m" >> $POSTBOOT_COMMANDS
+
+# JDBC pool
+echo "create-jdbc-connection-pool --restype javax.sql.ConnectionPoolDataSource \
+  --datasourceclassname org.postgresql.ds.PGConnectionPoolDataSource --isconnectvalidatereq true \
+  --validationmethod custom-validation  \
+  --validationclassname org.glassfish.api.jdbc.validation.PostgresConnectionValidation \
+  --property 'portNumber=${DB_PORT}:databaseName=${DB_NAME}:serverName=${DB_HOST}:user=${SORMAS_POSTGRES_USER}:password=${SORMAS_POSTGRES_PASSWORD}' sormasDataPool" >> $POSTBOOT_COMMANDS
+echo "create-jdbc-resource --connectionpoolid sormasDataPool jdbc/sormasDataPool" >> $POSTBOOT_COMMANDS
+
+# Pool for audit log
+echo "create-jdbc-connection-pool --restype javax.sql.XADataSource \
+  --datasourceclassname org.postgresql.xa.PGXADataSource --isconnectvalidatereq true \
+  --validationmethod custom-validation \
+  --validationclassname org.glassfish.api.jdbc.validation.PostgresConnectionValidation \
+  --property 'portNumber=${DB_PORT}:databaseName=${DB_NAME_AUDIT}:serverName=${DB_HOST}:user=${SORMAS_POSTGRES_USER}:password=${SORMAS_POSTGRES_PASSWORD}' sormasAuditlogPool" >> $POSTBOOT_COMMANDS
+echo "create-jdbc-resource --connectionpoolid sormasAuditlogPool jdbc/AuditlogPool" >> $POSTBOOT_COMMANDS
+
+echo "create-javamail-resource --mailhost localhost --mailuser user --fromaddress ${MAIL_FROM} mail/MailSession" >> $POSTBOOT_COMMANDS
+
+echo "create-custom-resource --restype java.util.Properties --factoryclass org.glassfish.resources.custom.factory.PropertiesFactory --property "org.glassfish.resources.custom.factory.PropertiesFactory.fileName=\${com.sun.aas.instanceRoot}/sormas.properties" sormas/Properties" >> $POSTBOOT_COMMANDS
+
+echo "create-jvm-options -Dlogback.configurationFile=\${com.sun.aas.instanceRoot}/config/logback.xml" >> $POSTBOOT_COMMANDS
+echo "set-log-attributes com.sun.enterprise.server.logging.GFFileHandler.maxHistoryFiles=14" >> $POSTBOOT_COMMANDS
+echo "set-log-attributes com.sun.enterprise.server.logging.GFFileHandler.rotationLimitInBytes=0" >> $POSTBOOT_COMMANDS
+echo "set-log-attributes com.sun.enterprise.server.logging.GFFileHandler.rotationOnDateChange=true" >> $POSTBOOT_COMMANDS
+
+echo "deploy /opt/payara/deployments/sormas-ear.ear" >> $POSTBOOT_COMMANDS
+echo "deploy /opt/payara/deployments/sormas-rest.war" >> $POSTBOOT_COMMANDS
+echo "deploy /opt/payara/deployments/sormas-ui.war" >> $POSTBOOT_COMMANDS
