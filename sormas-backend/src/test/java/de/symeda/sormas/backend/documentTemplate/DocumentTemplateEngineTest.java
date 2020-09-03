09daf5efa18d16ce51aa6b1b8413971ba7f558d5
syntakker <frank@boldsolutions.de>
2020-09-03 16:55:34 +0200

    test text generated by the template engine

--- sormas-backend/src/test/java/de/symeda/sormas/backend/documentTemplate/DocumentTemplateEngineTest.java
+++ sormas-backend/src/test/java/de/symeda/sormas/backend/documentTemplate/DocumentTemplateEngineTest.java
@@ -1,13 +1,17 @@
 package de.symeda.sormas.backend.documentTemplate;
 
 import fr.opensagres.xdocreport.core.XDocReportException;
+import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.junit.Test;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class DocumentTemplateEngineTest {
@@ -36,6 +40,12 @@ public class DocumentTemplateEngineTest {
         context.put("quarantine.to", "2020/09/03");
         context.put("quarantine.from", "2020/09/17");
 
-        DocumentTemplateEngine.generateDocument(context, filePath, ".");
+        String outFile = DocumentTemplateEngine.generateDocument(context, filePath, ".");
+
+        XWPFDocument outDocument = new XWPFDocument(new FileInputStream(outFile));
+        XWPFWordExtractor xwpfWordExtractor = new XWPFWordExtractor(outDocument);
+        String docxText = xwpfWordExtractor.getText();
+        assertEquals("Hello World Max Mustermann\n" +
+                "Quarantäne von 2020/09/17 bis 2020/09/03\n", docxText);
     }
 }
