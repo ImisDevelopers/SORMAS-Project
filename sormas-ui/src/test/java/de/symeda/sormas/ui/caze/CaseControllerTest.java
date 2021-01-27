3b549d9f6a4e804934f9c47b9a485cb645ec17da
Seal33 <klausbart@gmx.net>
2021-01-27 10:46:13 +0100

    Remove additional mock invocation

--- sormas-ui/src/test/java/de/symeda/sormas/ui/caze/CaseControllerTest.java
+++ sormas-ui/src/test/java/de/symeda/sormas/ui/caze/CaseControllerTest.java
@@ -1,25 +1,9 @@
 package de.symeda.sormas.ui.caze;
 
-import static org.mockito.Mockito.when;
-
-import java.security.Principal;
-
 import org.junit.Before;
-import org.mockito.Mockito;
 
-import com.vaadin.server.Page;
-import com.vaadin.server.VaadinRequest;
-import com.vaadin.server.VaadinService;
-import com.vaadin.server.VaadinServletRequest;
-import com.vaadin.server.VaadinSession;
-import com.vaadin.ui.UI;
-import com.vaadin.util.CurrentInstance;
-import com.vaadin.v7.data.util.converter.ConverterFactory;
-
-import de.symeda.sormas.api.user.UserRole;
 import de.symeda.sormas.ui.AbstractBeanTest;
 import de.symeda.sormas.ui.SormasUI;
-import de.symeda.sormas.ui.utils.SormasDefaultConverterFactory;
 
 public class CaseControllerTest extends AbstractBeanTest {
 
@@ -28,28 +12,24 @@ public class CaseControllerTest extends AbstractBeanTest {
 	@Before
 	public void initUI() throws Exception {
 
-		creator.createUser(null, null, null, "ad", "min", UserRole.ADMIN, UserRole.NATIONAL_USER);
-
-		VaadinRequest request = Mockito.mock(VaadinServletRequest.class);
-		when(request.getUserPrincipal()).thenReturn((Principal) () -> "admin");
-
-		CurrentInstance.set(VaadinRequest.class, request);
-
-		VaadinService service = Mockito.mock(VaadinService.class);
-		CurrentInstance.set(VaadinService.class, service);
-
-		VaadinSession session = Mockito.mock(VaadinSession.class);
-		ConverterFactory converterFactory = new SormasDefaultConverterFactory();
-		when(session.getConverterFactory()).thenReturn(converterFactory);
-		when(session.getService()).thenReturn(service);
-		CurrentInstance.set(VaadinSession.class, session);
-
-		ui = new SormasUI();
-		CurrentInstance.set(UI.class, ui);
-
-		java.lang.reflect.Field pageField = UI.class.getDeclaredField("page");
-		pageField.setAccessible(true);
-		pageField.set(ui, Mockito.mock(Page.class));
+		/*
+		 * creator.createUser(null, null, null, "ad", "min", UserRole.ADMIN, UserRole.NATIONAL_USER);
+		 * VaadinRequest request = Mockito.mock(VaadinServletRequest.class);
+		 * when(request.getUserPrincipal()).thenReturn((Principal) () -> "admin");
+		 * CurrentInstance.set(VaadinRequest.class, request);
+		 * VaadinService service = Mockito.mock(VaadinService.class);
+		 * CurrentInstance.set(VaadinService.class, service);
+		 * VaadinSession session = Mockito.mock(VaadinSession.class);
+		 * ConverterFactory converterFactory = new SormasDefaultConverterFactory();
+		 * when(session.getConverterFactory()).thenReturn(converterFactory);
+		 * when(session.getService()).thenReturn(service);
+		 * CurrentInstance.set(VaadinSession.class, session);
+		 * ui = new SormasUI();
+		 * CurrentInstance.set(UI.class, ui);
+		 * java.lang.reflect.Field pageField = UI.class.getDeclaredField("page");
+		 * pageField.setAccessible(true);
+		 * pageField.set(ui, Mockito.mock(Page.class));
+		 */
 	}
 
 //	@SuppressWarnings("unchecked")
